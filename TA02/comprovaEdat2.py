c2fafaae201cc0e213eb28d0fdb711134be03502
LluisSans-ITB2425 <lluis.sans.7e6@itb.cat>
2024-09-27 12:58:16 +0200

    comprovaDataNeixament

--- TA02/comprovaEdat2.py
+++ TA02/comprovaEdat2.py
@@ -1,19 +1,24 @@
-#Variables del any, mes i dia de neixament
+#Saber si ets major o menor d'edat fent servir l'import "datetime"
+#Lluis Sans
+#27/9/2024
+#ASIXc 1A
+
+import datetime
+
+# Variables del any, mes i dia de neixament
 any = int(input("Any de neixament:"))
 mes = int(input("Mes de neixament:"))
 dia = int(input("Dia de neixament:"))
 
-if any <= 2005:
-    print("Ets major d'edat")
+# Obté la data actual
+ara = datetime.datetime.now()
 
+# Determina si es major d'edad
+if any < ara.year - 18:
+    print("Ets major d'edat")
+elif mes < ara.month:
+    print("Ets major d'edat")
+elif dia <= ara.day:
+    print("Ets major d'edat")
 else:
-    if any == 2006:
-        if mes >= 9:
-            if dia <= 27:
-                print("Ets major d'edat")
-            else:
-                print("Ets menor")
-        else:
-            print("Ets major d'edat")
-    else:
-        print("Ets menor")
+    print("Ets menor")
